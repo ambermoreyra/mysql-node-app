5ce2fcae1151390b9bf0df2ac30672e2878e1694
Kona Marie <ambermoreyra@gmail.com>
2019-08-01 12:22:27 -0600

    completed functionality and readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mysql-node-app
+# "Bamazon" Shopping
+
+Bamazon is a command line app where the user interacts with items in the Bamazon database.
+
+## Bamazon Customer
+
+To shop, the user enters: `node bamazonCustomer`. Each item from the `products` table of the `bamazon_db`is listed including the ID, name and price.
+
+* Through Inquirer, the user is asked to identify the ID number and quantity of the item they would like to purchase.
+
+* If there is enough quantity in stock, then the purchase is made.
+* The database is updated to reflect the change in quantity and total sales for the product purchased.
+
+* If there is not enough quantity, the user will receive an insufficient quantity message.
+
+<img src="img/bamazon_example.jpg"> 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,53 +13,67 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("You're connected!");
-    getProductInfo();
     displayProducts();
-            
+
+
+
 })
 
-function displayProducts(){
-    connection.query("SELECT * FROM products", function(err, res){
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-// console.log(res);
         for (var i = 0; i < res.length; i++) {
             console.log(`${res[i].item_id} | ${res[i].product_name} | $${res[i].price}`)
-            }
-    
+        }
+        getProductInfo();
     })
+
 };
 
-function getProductInfo(){
+function updateQuantity(id, quantity) {
+    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [quantity, id], function(err, data) {
+        if (err) throw err;
+
+    })
+}
+function getProductInfo() {
 
 
-    inquirer.prompt([
-    {
-      name: "id",
-      type: "number",
-      message: "What is the ID number of the product you would like to buy?",
-    }, 
-    {
-    name: "quantity",
-    type: "number",
-    message: "How many units of the product would you like to buy?"
-    }
-]).then(function(answers) {
-      console.log(`Product ID of ${answers.id} in the quantity of ${answers.quantity} has been requested.`);
-      connection.query("SELECT item_id, stock_quantity FROM products WHERE ?",[
-          {
-          item_id: answers.id},
-         
-      ], 
-      function(err, data){
-          if (err) throw err;
-       console.log(data);
-      })
-  });
+    inquirer.prompt([{
+            name: "id",
+            type: "number",
+            message: "What is the ID number of the product you would like to buy?",
+        },
+        {
+            name: "quantity",
+            type: "number",
+            message: "How many units of the product would you like to buy?"
+        }
+    ]).then(function (answers) {
+        console.log(`Product ID of ${answers.id} in the quantity of ${answers.quantity} has been requested.`);
+        connection.query("SELECT * FROM products WHERE ?", [{
+                    item_id: answers.id
+                }
+
+            ],
+            function (err, data) {
+                if (err) throw err;
+                if (data[0].stock_quantity >= answers.quantity) {
+                    var totalCost = answers.quantity * data[0].price;
+                    console.log(`Your order for ${answers.quantity} ${data[0].product_name}(s) has been completed. Your total cost is ${totalCost}.`)
+                    var newQuantity = data[0].stock_quantity - answers.quantity;
+                    updateQuantity(data[0].item_id, newQuantity.toFixed(2));
+                    connection.end();
+                } else {
+                    console.log("Insufficient quantity!") /
+                        displayProducts();
+                }
+            }
+        );
+
+
+    });
 };
-
-
-
-
